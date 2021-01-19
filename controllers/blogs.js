2fd8e4e145d211a697a5508e46f2a8d9b005821b
Zoe Goldschmidt <zoe.gld@gmail.com>
2021-01-19 22:04:48 +0000

    Exercise 4.3

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -10,11 +10,11 @@ blogsRouter.get('/', (request, response) => {
 blogsRouter.post('/', (request, response, next) => {
   const blog = new Blog(request.body)
 
-    blog
-      .save()
-      .then(result => {
-        response.status(201).json(result)
-      })
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
     .catch(error => next(error))
 })
 
--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const http = require('http')
 const logger = require('./utils/logger')
 const config = require('./utils/config')
 
+const server = http.createServer(app)
+
 const PORT = 3003
-app.listen(config.PORT, () => {
+server.listen(config.PORT, () => {
   logger.info(`Server running on port ${PORT}`)
 })
