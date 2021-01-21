73b116a70965d5f72d470a511392d8417d1594ee
Zoe Goldschmidt <zoe.gld@gmail.com>
2021-01-21 17:39:17 +0000

    Exercise 4.13

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -93,6 +93,27 @@ describe('post /api/blogs', () => {
   })
 })
 
+describe('delete /api/blogs/:id', () => {
+  test('succeeds with status code 204 if id is valid', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToDelete = blogsAtStart[0]
+
+    await api
+      .delete(`/api/blogs/${blogToDelete.id}`)
+      .expect(204)
+
+    const blogsAtEnd = await helper.blogsInDb()
+
+    expect(blogsAtEnd).toHaveLength(
+      helper.initialBlogs.length - 1
+    )
+
+    const titles = blogsAtEnd.map(r => r.title)
+
+    expect(titles).not.toContain(blogToDelete.title)
+  })
+})
+
 afterAll(() => {
   mongoose.connection.close()
 })
