c78bf6fb8aed7a8c5749b2d38c3cc45bec11767a
Zoe Goldschmidt <zoe.gld@gmail.com>
2021-01-23 20:04:46 +0000

    Exercise 4.20

--- utils/middleware.js
+++ utils/middleware.js
@@ -1,5 +1,13 @@
 const logger = require('./logger')
 
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    request.token = authorization.substring(7)
+  }
+  next()
+}
+
 const requestLogger = (request, response, next) => {
   logger.info('Method:', request.method)
   logger.info('Path:  ', request.path)
@@ -30,6 +38,7 @@ const errorHandler = (error, request, response, next) => {
 }
 
 module.exports = {
+  tokenExtractor,
   requestLogger,
   unknownEndpoint,
   errorHandler
