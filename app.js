a3b5868ac790853cc4c4a156b1f0a2a28ac42fdf
Zoe Goldschmidt <zoe.gld@gmail.com>
2021-01-25 10:19:33 +0000

    Slight change in middlewares

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
 
-app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
+app.use(middleware.unknownEndpoint)
 
 module.exports = app
--- utils/middleware.js
+++ utils/middleware.js
@@ -21,6 +21,7 @@ const unknownEndpoint = (request, response) => {
 }
 
 const errorHandler = (error, request, response, next) => {
+  logger.error(error.message)
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
@@ -31,8 +32,6 @@ const errorHandler = (error, request, response, next) => {
       error: 'invalid token'
     })
   }
-
-  logger.error(error.message)
 
   next(error)
 }
